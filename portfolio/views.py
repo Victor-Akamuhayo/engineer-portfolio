458f276a63462626e9367218b58caf64bb9a93b4
Victor-Akamuhayo <isingomavictor26@gmail.com>
2026-08-21 11:14:41 +0300

    Update portfolio deployment

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render
-from .models import Profile, Certificate, Project, Experience, Education, Skill 
+﻿from types import SimpleNamespace
 
-    
+from django.db.utils import OperationalError
+from django.shortcuts import render
+
+from .models import Certificate, Education, Experience, Profile, Project, Skill
+
+
+def _fallback_profile():
+    return SimpleNamespace(
+        title='Mining Engineer',
+        name='Eng. Tenywa Ibrahim',
+        bio='Professional portfolio and project showcase.',
+        profile_photo=None,
+        cv=None,
+        current_position='',
+        years_of_experience=0,
+        location='',
+        email='',
+        phone='',
+        linkedin='',
+    )
+
+
 def home(request):
-    profile = Profile.objects.first()
-    certificates = Certificate.objects.all()
-    experiences = Experience.objects.all()
-    education = Education.objects.all()
-    projects = Project.objects.all()
-    skills = Skill.objects.all()
+    try:
+        profile = Profile.objects.first() or _fallback_profile()
+        certificates = Certificate.objects.all()
+        experiences = Experience.objects.all()
+        education = Education.objects.all()
+        projects = Project.objects.all()
+        skills = Skill.objects.all()
+    except OperationalError:
+        profile = _fallback_profile()
+        certificates = []
+        experiences = []
+        education = []
+        projects = []
+        skills = []
 
     context = {
         'profile': profile,
@@ -19,8 +47,4 @@ def home(request):
         'skills': skills,
     }
 
-    return render(
-        request,
-        'portfolio/home.html',
-        context
-    )
+    return render(request, 'portfolio/home.html', context)
